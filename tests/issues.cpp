03c49b2a8c173f4f8633d5d61a2904937106b4ba
xzl <xiangzelong@deepin.org>
2023-11-16 09:54:10 +0800

    chore: update to 1.1.0-1
    Log:

--- tests/issues.cpp
+++ tests/issues.cpp
@@ -1,7 +1,8 @@
-#include "catch.hpp"
+#include <catch2/catch.hpp>
 #include <tl/expected.hpp>
 
 #include <string>
+#include <memory>
 
 using std::string;
 
@@ -15,7 +16,7 @@ TEST_CASE("Issue 1", "[issues.1]") { getInt1(); }
 
 tl::expected<int, int> operation1() { return 42; }
 
-tl::expected<std::string, int> operation2(int const val) { return "Bananas"; }
+tl::expected<std::string, int> operation2(int const val) { (void)val; return "Bananas"; }
 
 TEST_CASE("Issue 17", "[issues.17]") {
   auto const intermediate_result = operation1();
@@ -67,7 +68,7 @@ struct i31{
 };
 TEST_CASE("Issue 31", "[issues.31]") {
     const tl::expected<i31, int> a = i31{42};
-    a->i;
+    (void)a->i;
 
     tl::expected< void, std::string > result;
     tl::expected< void, std::string > result2 = result;
@@ -77,7 +78,7 @@ TEST_CASE("Issue 31", "[issues.31]") {
 TEST_CASE("Issue 33", "[issues.33]") {
     tl::expected<void, int> res {tl::unexpect, 0};
     REQUIRE(!res);    
-    res = res.map_error([](int i) { return 42; });
+    res = res.map_error([](int i) { (void)i; return 42; });
     REQUIRE(res.error() == 42);
 }
 
@@ -127,3 +128,68 @@ TEST_CASE("Issue 49", "[issues.49]") {
     .and_then(test2);
 }
 #endif
+
+tl::expected<int, std::unique_ptr<std::string>> func()
+{
+  return 1;
+}
+
+TEST_CASE("Issue 61", "[issues.61]") {
+  REQUIRE(func().value() == 1);
+}
+
+struct move_tracker {
+        int moved = 0;
+
+        move_tracker() = default;
+
+        move_tracker(move_tracker const &other) noexcept {};
+        move_tracker(move_tracker &&orig) noexcept
+            : moved(orig.moved + 1) {}
+
+        move_tracker &
+        operator=(move_tracker const &other) noexcept {};
+        
+        move_tracker &operator=(move_tracker &&orig) noexcept {
+          moved = orig.moved + 1;
+          return *this;
+        }
+};
+
+TEST_CASE("Issue 122", "[issues.122]") { 
+     tl::expected<move_tracker, int> res;
+     res.emplace();
+     REQUIRE(res.value().moved == 0);
+}
+
+#ifdef __cpp_deduction_guides
+TEST_CASE("Issue 89", "[issues.89]") { 
+    auto s = tl::unexpected("Some string");
+    REQUIRE(s.value() == std::string("Some string"));
+}
+#endif
+
+struct S {
+    int i = 0;
+    int j = 0;
+    S(int i) : i(i) {}
+    S(int i, int j) : i(i), j(j) {}
+};
+
+TEST_CASE("Issue 107", "[issues.107]") {
+    tl::expected<int, S> ex1(tl::unexpect, 2); 
+    tl::expected<int, S> ex2(tl::unexpect, 2, 2);
+
+    REQUIRE(ex1.error().i == 2);
+    REQUIRE(ex1.error().j == 0);
+    REQUIRE(ex2.error().i == 2);
+    REQUIRE(ex2.error().j == 2);
+}
+
+TEST_CASE("Issue 129", "[issues.129]") {
+  tl::expected<std::unique_ptr<int>, int> x1 {std::unique_ptr<int>(new int(4))};
+  tl::expected<std::unique_ptr<int>, int> y1 {std::unique_ptr<int>(new int(2))};
+  x1 = std::move(y1);
+
+  REQUIRE(**x1 == 2);
+}
